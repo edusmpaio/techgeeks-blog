629ca4e5f3b5783b3cf3f52b7f295f340435d5f3
mikaellpc <60710335+mikaellpc4@users.noreply.github.com>
2023-10-06 16:59:12 -0300

    FEAT: New Card Style

--- src/utils/calculatePostDate.ts
+++ src/utils/calculatePostDate.ts
@@ -7,19 +7,24 @@ export const calculatePostDate = (date: Date) => {
   const hours = Math.floor(remainingSeconds / 3600)
   const minutes = Math.floor((remainingSeconds % 3600) / 60)
 
-  let result = ''
-
   if (days > 0) {
-    result += `${days}d `
+    if (days > 1) {
+      return `${days} dias `
+    }
+    return `${days} dia `
   }
 
-  if (hours > 0) {
-    result += `${hours}h `
+  if (hours > 0 && days < 1) {
+    if (hours > 1) {
+      return `${hours} horas `
+    }
+    return `${hours} hora `
   }
 
-  if (minutes > 0) {
-    result += `${minutes}m `
+  if (minutes > 0 && hours < 1) {
+    if (minutes > 1) {
+      return `${minutes} minutos `
+    }
+    return `${minutes} minuto `
   }
-
-  return result.trim()
 }
