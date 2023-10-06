a9c62965a17b2871c52abb16bb5323cb09a1a54f
mikaellpc <60710335+mikaellpc4@users.noreply.github.com>
2023-10-06 02:20:56 -0300

    FEAT: Add Notion Integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { notion } from '@/utils/api'
+import Image from 'next/image'
+import { calculatePostDate } from '@/utils/calculatePostDate'
+import { calculateReadingTime } from '@/utils/calculateReadingTime'
 
-export default function Home() {
+export default async function Home() {
+  const databaseId = 'f15652bb6bb446b287d62ee7bdb91fb3'
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  })
+  const posts = response.results as Post[]
+
+  if (!posts) return null
+
   return (
     <main>
       <section>
@@ -27,35 +39,38 @@ export default function Home() {
             Últimas postagens
           </strong>
           <div className="grid gap-9 md:grid-cols-2">
-            <Link href="/">
-              <div className="aspect-video w-full rounded-lg bg-zinc-900" />
+            {posts.map((post) => {
+              const dateDiff = calculatePostDate(new Date(post.created_time))
+              const readingTime = calculateReadingTime(
+                post.properties.Conteúdo.rich_text[0].text.content,
+              )
+              const image = post.properties['Imagem Capa'].files[0]?.file.url
+              return (
+                <Link key={post.id} href="/">
+                  <div className="relative aspect-video w-full overflow-hidden rounded-xl bg-zinc-900">
+                    <div className="absolute w-full bg-zinc-900/40" />
+                    {image && (
+                      <Image src={image} alt="" width={700} height={450} />
+                    )}
+                  </div>
 
-              <strong className="mb-4 mt-8 block text-xl sm:max-md:text-2xl lg:text-2xl">
-                A Revolução da Inteligência Artificial: O Que Esperar em 2024
-              </strong>
-              <p className="mb-11 line-clamp-2 leading-relaxed text-muted-foreground">
-                Descubra as tendências da IA para o próximo ano e seu impacto em
-                várias indústrias.
-              </p>
-              <p className="text-sm text-muted-foreground/80">
-                <span>5 min de leitura</span> • <span>há 2 dias</span>
-              </p>
-            </Link>
-
-            <Link href="/">
-              <div className="aspect-video w-full rounded-lg bg-zinc-900" />
-
-              <strong className="mb-4 mt-8 block text-xl sm:max-md:text-2xl lg:text-2xl">
-                Os Avanços da Realidade Aumentada em Aplicações do Cotidiano
-              </strong>
-              <p className="mb-11 line-clamp-2 leading-relaxed text-muted-foreground">
-                Explore como a realidade aumentada está mudando nossas vidas
-                diárias e o futuro tecnológico
-              </p>
-              <p className="text-sm text-muted-foreground/80">
-                <span>3 min de leitura</span> • <span>há 15 horas</span>
-              </p>
-            </Link>
+                  <strong className="mb-4 mt-8 block text-xl sm:max-md:text-2xl lg:text-2xl">
+                    {post.properties.Nome.title[0].text.content}
+                  </strong>
+                  <p className="mb-11 line-clamp-2 leading-relaxed text-muted-foreground">
+                    {post.properties.Conteúdo.rich_text[0].text.content}
+                  </p>
+                  <p className="text-sm text-muted-foreground/80">
+                    <span>
+                      {readingTime < 1
+                        ? 'Menos de 1 minuto'
+                        : `${readingTime} minuto`}
+                    </span>{' '}
+                    •<span>há {dateDiff}</span>
+                  </p>
+                </Link>
+              )
+            })}
           </div>
         </div>
       </section>
